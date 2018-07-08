42ea07dd1b733e0c66f9d0bb148135f6f0b1c387
Andy5am <38666859+Andy5am@users.noreply.github.com>
2018-07-08 14:34:25 -0600

    variables clase radio

--- src/radio.kt
+++ src/radio.kt
@@ -1,5 +1,11 @@
 package main
 
 fun main (args: Array<String>){
-    
+    class radio(
+            val encendido: Boolean = false,
+            var volumen: Int,
+            var emisora: String,
+            var NumEmisora: Float,
+
+    )
 }
