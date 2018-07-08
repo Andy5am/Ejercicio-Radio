054e0a00e47c7bf825d440adc53bde54a42263db
Andy5am <38666859+Andy5am@users.noreply.github.com>
2018-07-08 15:49:08 -0600

    clase

--- src/radio.kt
+++ src/radio.kt
@@ -1,11 +1,47 @@
 package main
 
 fun main (args: Array<String>){
-    class radio(
-            val encendido: Boolean = false,
-            var volumen: Int,
-            var emisora: String,
-            var NumEmisora: Float,
+    var radio = radio(false,20, "Am",99.9 )
+    println(radio)
 
-    )
+    radio.encender()
+    radio.ponerFm()
+
+    println(radio)
+
+
+
+
+}
+class radio(
+        var encendido: Boolean = false,
+
+        var volumen: Int,
+        var emisora: String,
+        var NumEmisora: Double
+        ){
+    fun encender(){
+        encendido= true
+    }
+    fun apagar(){
+        encendido= false
+    }
+    fun ponerAm(){
+        emisora = "Am"
+    }
+    fun ponerFm(){
+        emisora="Fm"
+    }
+    fun ajustarVol(){
+        volumen = readLine("")
+    }
+    override fun toString(): String {
+        return """
+            Radio:
+                Esta encendido: $encendido
+                Esta en: $emisora
+                Sintoniza la emisora: $NumEmisora
+                El volumen es de: $volumen
+        """.trimIndent()
+    }
 }
