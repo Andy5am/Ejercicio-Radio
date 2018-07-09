f45976225b2fac4b87bfd27fbe23299ec7b55f94
Andy5am <38666859+Andy5am@users.noreply.github.com>
2018-07-08 18:15:07 -0600

    menu

--- src/radio.kt
+++ src/radio.kt
@@ -1,39 +1,78 @@
 package main
 
-fun main (args: Array<String>){
-    var radio = radio(false,20, "Am",99.9 )
+fun main (args: Array<String>) {
+    var radio = Radio(false, 20, "Am", 99.9)
     println(radio)
 
-    radio.encender()
-    radio.ponerFm()
+    println("""
+       Radio:
+       1. Encender
+       2. Salir
+   """.trimIndent())
+    var opcion = "0"
+    while (opcion != "2") {
+        print("Que opcion desea?:")
+        var opcion = readLine()!!
+        if (opcion == "1") {
+            println("""
+                Radio:
+                1. Cambiar a Am
+                2. Cambiar a Fm
+                3. Subir volumen
+                4. Bajar volumen
+                5. Subir emisora
+                6. Bajar emisora
+                7. Apagar
+            """.trimIndent())
+            var opcion2 = "0"
+            while (opcion2 != "7")
+            println("Que opcion desea?: ")
+            var opcion2 = readLine()!!
+            if (opcion2 == "1") {
+                radio.ponerAm()
+                println(radio)
+            } else if (opcion2 == "2") {
+                radio.ponerFm()
+                println(radio)
+            } else if (opcion2 == "3") {
+                radio.subirvol()
+                println(radio)
+            } else if (opcion2 == "4") {
+                radio.bajarvol()
+                println(radio)
+            } else if (opcion2 == "5") {
+            }
+        }
 
-    println(radio)
 
-
-
-
+    }
 }
-class radio(
+class Radio(
         var encendido: Boolean = false,
-
         var volumen: Int,
         var emisora: String,
         var NumEmisora: Double
-        ){
-    fun encender(){
-        encendido= true
+        ) {
+    fun encender() {
+        encendido = true
     }
-    fun apagar(){
-        encendido= false
+
+    fun apagar() {
+        encendido = false
     }
-    fun ponerAm(){
+
+    fun ponerAm() {
         emisora = "Am"
     }
-    fun ponerFm(){
-        emisora="Fm"
+
+    fun ponerFm() {
+        emisora = "Fm"
     }
-    fun ajustarVol(){
-        volumen = readLine("")
+    fun subirvol(){
+        volumen+= 1
+    }
+    fun bajarvol(){
+        volumen-=1
     }
     override fun toString(): String {
         return """
